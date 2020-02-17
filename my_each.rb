c0ceb703b53ddb50918c77bda3863ddac51770bc
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-17 02:40:57 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,12 @@
 require 'pry'
 
-def my_each(array)
-  i = 0
-  while i < array.length
-  yield(array[i])
-  i = i = 1
-  binding.pry
-
-  # code here
-end
+  def my_each(array)
+    i = 0
+    while i < array.length
+    yield(array[i])
+    i = i = 1
+    binding.pry
+  
+    # code here
+  end
 end
